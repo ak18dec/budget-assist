dacdfd6e70c389d5ea08af004ef40050fda3b591
ak18dec <ankitdayal1992@gmail.com>
2026-03-13 19:41:03 +0530

    fix message hover copy icon issue

--- frontend/src/components/chatsidebar/ChatMessage.jsx
+++ frontend/src/components/chatsidebar/ChatMessage.jsx
@@ -7,7 +7,7 @@ import { Copy, Check } from 'lucide-react';
 export default function ChatMessage({message, copiedId, handleCopy}) {
   const isUser = message.sender === "user"
   return (
-    <div className={`flex gap-3 ${isUser ? "justify-end" : ""}`}>
+    <div className={`group/message flex gap-3 ${isUser ? "justify-end" : ""}`}>
       <div
         className={`group max-w-[75%] min-w-[50%] rounded-lg px-3 py-2 text-xs
         ${ isUser ? "bg-primary text-primary-foreground" : "bg-muted" }`}
@@ -20,7 +20,7 @@ export default function ChatMessage({message, copiedId, handleCopy}) {
           <Button
             size="icon"
             variant="ghost"
-            className="opacity-0 group-hover:opacity-100 h-2 w-2"
+            className="opacity-0 group-hover/message:opacity-100 h-2 w-2 cursor-pointer"
             onClick={() => handleCopy(message.text, message.id)}
           >
             {copiedId === message.id ? <Check size={4}/> : <Copy size={4}/>}
